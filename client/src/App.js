d445ba594cc87af67670f8ee3cc176f581bca457
fitsum belayneh <fitsu_alt@yahoo.com>
2018-08-09 14:17:36 -0400

    modal

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
 import ShopingList from './components/shoppinglist';
+import ItemModal from './components/itemModal';
+import { Container } from 'reactstrap';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -14,7 +16,10 @@ class App extends Component {
       <Provider store={store}>
         <div className="App">
           <AppNavbar />
-          <ShopingList/>
+          <Container>
+            <ItemModal/>
+            <ShopingList/>
+          </Container>
         </div>
       </Provider>
     );
