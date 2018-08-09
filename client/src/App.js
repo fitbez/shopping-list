d143bc7a5b84dd16fdf94789dec057abc308a8f9
fitsum belayneh <fitsu_alt@yahoo.com>
2018-08-09 01:08:00 -0400

    transition from state to props

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
-import AppNavbar from './components/AppNavbar'
+import AppNavbar from './components/AppNavbar';
 import ShopingList from './components/shoppinglist';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavbar />
-        <ShopingList/>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <ShopingList/>
+        </div>
+      </Provider>
     );
   }
 }
